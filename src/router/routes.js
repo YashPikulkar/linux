8794873f0cf11fddb6ddf283abaaae167a38b211
Vanetta P Silveira <149812160+Vanettas@users.noreply.github.com>
2025-07-27 12:23:42 +0530

    Add files via upload

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,24 +9,35 @@ export default [
       },
       {
         path: "jobs",
-        component: () => import("src/pages/JobDashboard.vue"), // 👈 new route
+        component: () => import("pages/JobDashboard.vue"),
       },
-
       {
         path: "register",
-        component: () => import("src/pages/RegisterForm.vue"), // 👈 new route
+        component: () => import("pages/RegisterForm.vue"),
       },
       {
-        path: "/userdashboard",
-        component: () => import("src/pages/UserDashboard.vue"),
+        path: "userdashboard",
+        component: () => import("pages/UserDashboard.vue"),
       },
       {
-        path: "/recruiterdashboard",
-        component: () => import("src/pages/RecruiterDashboard.vue"),
+        path: "recruiterdashboard",
+        component: () => import("pages/RecruiterDashboard.vue"),
       },
       {
-        path: "/login",
-        component: () => import("src/pages/login.vue"),
+        path: "login",
+        component: () => import("pages/login.vue"),
+      },
+
+      // ✅ Separate Candidate Settings Page
+      /*{
+        path: "settings/candidate",
+        component: () => import("pages/settings/candidate/Settings.vue"),
+      },*/
+
+      // ✅ Separate Recruiter Settings Page
+      {
+        path: "recruiter/settings",
+        component: () => import("src/components/RecruiterDashboard/settings/settings.vue"),
       },
     ],
   },
